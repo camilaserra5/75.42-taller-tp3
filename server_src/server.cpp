02cb7dcbb8630434a69cbec59813c1b6b8115f4e
Cami Serra <camilaserra5@gmail.com>
2020-11-08 20:15:40 -0300

    improve client handling

--- server_src/server.cpp
+++ server_src/server.cpp
@@ -1,10 +1,29 @@
 #include <vector>
 #include <iostream>
 #include "server.h"
+#include "client_processor.h"
 
 Server::Server(Socket &socket, std::string rootFile) : socket(std::move(socket)), rootFile(rootFile) {
+
 }
 
 
 void Server::run() {
+    while (true) {
+        Socket client = this->socket.accept();
+        this->clients.push_back(new ClientProcessor(client));
+        this->clients.back()->start();
+        for (ClientProcessor *cli : this->clients) {
+            if (!cli->isAlive()) {
+                delete cli;
+            }
+        }
+    }
+}
+
+Server::~Server() {
+    for (ClientProcessor *cli : this->clients) {
+        delete cli;
+    }
+    this->join();
 }
--- server_src/server.h
+++ server_src/server.h
@@ -2,16 +2,24 @@
 #define SERVER_H
 
 #include <string>
+#include <vector>
 #include "../common_src/socket.h"
+#include "../common_src/thread.h"
+#include "client_processor.h"
 
-class Server {
+class Server : public Thread {
 public:
-    Server(Socket& socket, std::string rootFile);
+    Server(Socket &socket, std::string rootFile);
 
-    void run();
+    void run() override;
+
+    ~Server() override;
 
 private:
     Socket socket;
+
+    std::vector<ClientProcessor *> clients;
+
     std::string rootFile;
 };
 
